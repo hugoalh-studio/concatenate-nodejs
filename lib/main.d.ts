b473f340042ef9667a286de8f7bada517d1cd924
hugoalh <32359235+hugoalh@users.noreply.github.com>
2022-10-17 11:52:10 +0800

    Bump version

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -3,7 +3,7 @@ export = concatenate;
  * @function concatenate
  * @description Concatenate multiple items into one item.
  * @param {...(any[]|Map<any,any>|object|Set<any>|string)} items Items that need to concatenate into one.
- * @returns {(any[]|Map<any,any>|object|Set<any>|string)} A concatenated item.
+ * @returns {any[]|Map<any,any>|object|Set<any>|string} A concatenated item.
  */
-declare function concatenate(...items: (any[] | Map<any, any> | object | Set<any> | string)[]): (any[] | Map<any, any> | object | Set<any> | string);
+declare function concatenate(...items: (any[] | Map<any, any> | object | Set<any> | string)[]): any[] | Map<any, any> | object | Set<any> | string;
 //# sourceMappingURL=main.d.ts.map
